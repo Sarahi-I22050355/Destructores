e9c2d52d91ca546c43633100e73da4086e4cdfcf
Sarah <Sarah@HP-SARAHI>
2023-03-09 22:35:04 -0600

    Agregar archivos de proyecto.

---
+++ ManejoArchivos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace Destructores
+{
+    internal class ManejoArchivos
+    {
+        StreamReader archivo = null;
+
+        int contador = 0;
+
+        string linea;
+
+        public ManejoArchivos() 
+        {
+            archivo = new StreamReader(@"C:\Users\Sarah\OneDrive\Escritorio\data.txt");
+            while ((linea = archivo.ReadLine())!=null)
+            {
+                Console.WriteLine(linea);
+                contador++;
+            }
+        }
+        public void Mensaje()
+        {
+            Console.WriteLine("Hay {0} lineas",contador);
+
+        }
+        //El codigo interno de un destructor es para que se ejecute inmediatamnte
+        //despues de aver eliminado el recolector de basura el recurso correspondiente
+        ~ManejoArchivos()
+        {
+            archivo.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Destructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miarchivo = new ManejoArchivos();
+            miarchivo.Mensaje();
+            Console.ReadKey();
+        }
+        
+    }
+}
